296e748853d2be1f3dd4396fd0651b088f22c2b9
フランカツ!! <kole3600@gmail.com>
2024-04-12 12:46:50 -0700

    done
    done

--- debug6.py
+++ debug6.py
@@ -4,4 +4,8 @@ Debug this program so that it runs
 expected output:
 A girl says, "Valar Morgulis"
 """
-print("A girl says, "Valar Morgulis"")
+print("A girl says, \"Valar Morgulis\"")
+
+## alternatively,
+
+print('A girl says, "Valar Morgulis"')
--- debug9.py
+++ debug9.py
@@ -2,7 +2,9 @@
 """
 Debug this program so that it runs
 """
-x = input("Enter a number")
-y = 2
-answer = x+y
-print(The answer is + answer)
+x = int(input("Enter a number"))
+y = int(2)
+answer = x + y
+print(f"The answer for is 2 + {x} is {answer}")
+
+##the math did not seem to make sense here so i tried to correct it, however im not sure if that is what you wanted
